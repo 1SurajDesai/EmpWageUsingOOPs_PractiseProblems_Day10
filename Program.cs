bb6504ad3860281cc3b0aedec52be1e20ba37272
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 15:00:18 +0530

    Adding program.cs file

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace EmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            //EmployeeCheck empcheck = new EmployeeCheck();
+            //empcheck.EmployeeAbsentOrPresent();
+            //EmpyeeWage.CalculateEmpWage();
+            //PartTimeEmpWage.CalEmpWage();
+            //WageUsingSwitchCase.CalWageSwitchCase();
+            //WageForMonth.CalWageOfMonth();
+            //CalWageTillCondMeet.CalWage();
+
+        }
+    }
+}
