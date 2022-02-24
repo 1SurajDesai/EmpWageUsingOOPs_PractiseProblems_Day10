e655dc51192f17b57ef6b961fa5ef2b99e887223
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 15:02:54 +0530

    Adding program to check whether employee is present or not

---
+++ EmployeeCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeCheck
+    {
+        public void EmployeeAbsentOrPresent()
+        {
+            int num;
+            Random random = new Random();
+            num = random.Next(2);
+            if (num == 1)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
