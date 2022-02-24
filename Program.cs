f4efab0d10411d4ca137cad398293a1d61ff3895
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 15:26:26 +0530

    Updating Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-namespace EmployeeWageProblem
+namespace EmployeeWageProblemWithObject
 {
     public class Program
     {
         public static void Main(string[] arg)
         {
             //Console.WriteLine("Welcome to Employee Wage Computation Program ");
-            EmployeeCheck empcheck = new EmployeeCheck();
-            empcheck.EmployeeAbsentOrPresent();
-            
+            //EmployeeCheck empcheck = new EmployeeCheck();
+            //.EmployeeAbsentOrPresent();
+            EmpyeeWage empwage = new EmpyeeWage();
+            empwage.CalculateEmpWage();
 
         }
     }
