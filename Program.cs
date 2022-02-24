70286c4e2de6b384ba663cac4a0a8491a5c4c962
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 15:04:32 +0530

    Updating Program.cs file

--- Program.cs
+++ Program.cs
@@ -5,14 +5,10 @@ namespace EmployeeWageProblem
     {
         public static void Main(string[] arg)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program ");
-            //EmployeeCheck empcheck = new EmployeeCheck();
-            //empcheck.EmployeeAbsentOrPresent();
-            //EmpyeeWage.CalculateEmpWage();
-            //PartTimeEmpWage.CalEmpWage();
-            //WageUsingSwitchCase.CalWageSwitchCase();
-            //WageForMonth.CalWageOfMonth();
-            //CalWageTillCondMeet.CalWage();
+            //Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            EmployeeCheck empcheck = new EmployeeCheck();
+            empcheck.EmployeeAbsentOrPresent();
+            
 
         }
     }
